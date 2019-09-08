7d3de52704b5bef2f7017645021b93d7b7b9e011
afozbek <furkanozbek1995@gmail.com>
2019-09-08 12:31:13 +0300

    adding Header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row mt-5">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<script>
+import HeaderVue from "./components/Header.vue";
+
+export default {
+  components: {
+    appHeader: HeaderVue
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  padding: 30px;
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,48 @@
 <template>
-  <h1>Header Component</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/portfolio"
+            >Portfolio</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" active-class="active" to="/stocks"
+            >Stocks</router-link
+          >
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <router-link class="nav-link" to>End Day</router-link>
+        </li>
+        <li>
+          <div class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Save / Load
+              <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Save Data</a>
+              <a class="dropdown-item" href="#">Load Data</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
