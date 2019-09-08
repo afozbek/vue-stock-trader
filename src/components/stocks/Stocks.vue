ab2ee808e405bf9b4ecf01bb2d404512f9ce49e2
afozbek <furkanozbek1995@gmail.com>
2019-09-08 14:00:08 +0300

    updating Stocks.vue

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,55 @@
 <template>
-  <h1>Stocks/Stocks Component</h1>
+  <vue-fragment>
+    <app-stock
+      v-for="stock in stocks"
+      :key="stock.id"
+      :name="stock.name"
+      :price="stock.price"
+    ></app-stock>
+  </vue-fragment>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock.vue";
+import { Fragment } from "vue-fragment";
+
+export default {
+  data() {
+    return {
+      stocks: [
+        {
+          id: 1,
+          name: "BMW",
+          price: 110
+        },
+        {
+          id: 2,
+          name: "Google",
+          price: 200
+        },
+        {
+          id: 3,
+          name: "Apple",
+          price: 250
+        },
+        {
+          id: 4,
+          name: "Twitter",
+          price: 8
+        },
+        {
+          id: 5,
+          name: "Youtube",
+          price: 8
+        }
+      ]
+    };
+  },
+  components: {
+    appStock: Stock,
+    vueFragment: Fragment
+  }
+};
 </script>
 
 <style></style>
