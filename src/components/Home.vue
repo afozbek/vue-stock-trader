892c45833fbc7ac40101aad7ce9a9e777cb05ef0
afozbek <furkanozbek1995@gmail.com>
2019-09-08 16:35:25 +0300

    adding home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>Home Component</h1>
+  <div>
+    <h1>Trade or View your Portfolio</h1>
+    <h6>You may Save & Load your Data</h6>
+    <h6>Click on 'End Day' to begin a New Day!</h6>
+    <hr />
+    <p>Your funds: {{ funds }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["funds"])
+  }
+};
 </script>
 
 <style></style>
