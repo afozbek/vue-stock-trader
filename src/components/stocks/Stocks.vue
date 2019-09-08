8cf3391d833e036ff33519babc7befe44d3bb96c
afozbek <furkanozbek1995@gmail.com>
2019-09-08 15:15:27 +0300

    removing dummy data

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -11,38 +11,11 @@
 <script>
 import Stock from "./Stock.vue";
 import { Fragment } from "vue-fragment";
+import { mapGetters } from "vuex";
 
 export default {
-  data() {
-    return {
-      stocks: [
-        {
-          id: 1,
-          name: "BMW",
-          price: 110
-        },
-        {
-          id: 2,
-          name: "Google",
-          price: 200
-        },
-        {
-          id: 3,
-          name: "Apple",
-          price: 250
-        },
-        {
-          id: 4,
-          name: "Twitter",
-          price: 8
-        },
-        {
-          id: 5,
-          name: "Youtube",
-          price: 8
-        }
-      ]
-    };
+  computed: {
+    ...mapGetters(["stocks"])
   },
   components: {
     appStock: Stock,
