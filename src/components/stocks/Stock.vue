46710ce6e968afbf0bf72be90f135540aca56e54
afozbek <furkanozbek1995@gmail.com>
2019-09-08 13:59:41 +0300

    adding new props

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>Stocks/Stock Component</h1>
+  <div class="col-lg-4 col-sm-6 col-12">
+    <div class="card mt-3 border-success">
+      <div class="card-header text-white bg-success">
+        <h4 class="card-title">
+          {{ name }}
+          <small>(Price: {{ price }})</small>
+        </h4>
+      </div>
+      <div class="card-body">
+        <div class="float-left">
+          <input type="number" class="form-control" placeholder="Quantity" />
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["name", "price"]
+};
 </script>
 
 <style></style>
