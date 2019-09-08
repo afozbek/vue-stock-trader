4944269aafd692bda8a29b747f14e25138d23129
afozbek <furkanozbek1995@gmail.com>
2019-09-08 15:33:56 +0300

    dispatching buyStock

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -43,10 +43,11 @@ export default {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
-        quantity: this.quantity
+        quantity: +this.quantity
       };
       console.log(order);
 
+      this.$store.dispatch("buyStock", order);
       this.quantity = 0;
     }
   },
